74f2c368aff9011994540f75b465124c7670090b
azukichi67 <>
2025-03-08 14:10:43 +0900

    init 2

--- README.md
+++ README.md
@@ -1,7 +1,5 @@
 # sandbox-tanstack
 
 ```
-npm create vite@latest
-npm install
-npm i @tanstack/react-router
+npm create @tanstack/router@latest
 ```
--- vite.config.ts
+++ vite.config.ts
@@ -1,9 +1,16 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { TanStackRouterVite } from "@tanstack/router-plugin/vite";
 
-// https://vite.dev/config/
+// https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    TanStackRouterVite({
+      target: "react",
+      autoCodeSplitting: true,
+    }),
+    react(),
+  ],
   server: {
     host: "0.0.0.0",
     watch: {
